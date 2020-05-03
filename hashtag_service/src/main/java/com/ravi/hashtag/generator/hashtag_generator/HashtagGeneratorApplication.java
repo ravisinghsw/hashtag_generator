710e270a41f2c666853093d136c551a51c7a81b0
ravi <ravisinghsw@gmail.com>
2020-05-03 20:35:14 +0530

    initial commit

---
+++ hashtag_service/src/main/java/com/ravi/hashtag/generator/hashtag_generator/HashtagGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.hashtag.generator.hashtag_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashtagGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashtagGeneratorApplication.class, args);
+	}
+
+}
---
+++ hashtag_service/src/test/java/com/ravi/hashtag/generator/hashtag_generator/HashtagGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.hashtag.generator.hashtag_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashtagGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
